ab3571b4bdf14b998f25d13814ab7c553d2eea5c
Tonny Mutai <tonnimutai@gmail.com>
2022-02-11 16:00:52 +0300

    switch to find querying

--- src/services/db.ts
+++ src/services/db.ts
@@ -11,7 +11,6 @@ export class DbService {
 	private readonly db = new PouchDb(config.get('POUCHDB_URL'));
 
 	constructor() {
-		// Indexing for pouchdb??
     this.db.createIndex({
       index: {
         fields: ['challenger', 'opponent', 'username']
@@ -19,34 +18,37 @@ export class DbService {
     })
 	}
 
-
-
-	async create (data: any): Promise<any> {
+	async create (data: any, returnOptions?: any): Promise<any> {
 		logger.info('DbService:create: Create a new entity');
 
 		const result = await this.db.post(data);
-		const entity = await this.db.get(result.id);
+		const [entity] = await this.find({
+			_id: result.id
+		}, returnOptions);
 
 		return entity;
 	}
 
-	async update(id: string, data: any): Promise<any> {
+	async update(id: string, data: any, returnOptions?: any): Promise<any> {
 		logger.info('DbService:update - update entity');
 		const entity = await this.db.get(id);
 
 		await this.db.put({ ...entity, ...data });
 
-		return this.db.get(entity._id);
+		const [doc] = await this.find({ _id: entity._id }, returnOptions);
+		return doc;
 	}
 
-  async getOne(id: string): Promise<any> {
-    return this.db.get(id);
+  async getOne(id: string, returnOptions?: any): Promise<any> {
+    const [doc] = await this.find({ _id: id }, returnOptions);
+		return doc;
   }
 
-	async find(query: any): Promise<any> {
+	async find(query: any, returnOptions?: any): Promise<any> {
 		logger.info('DbService:find - query for entities');
 		const docs = await this.db.find({
 			selector: query,
+			...returnOptions,
 		});
 
 		return docs.docs;
