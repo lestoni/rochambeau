b239d13d1c42255e4564906c527f4ff5737773bd
Tonny Mutai <tonnimutai@gmail.com>
2022-02-16 16:57:54 +0300

    add NODE_ENV to docker builder

--- tools/build-docker.sh
+++ tools/build-docker.sh
@@ -4,4 +4,4 @@ docker build . -t tonni_mutai/rochambeau
 
 echo 'docker image built ...'
 
-docker run --rm -p 8000:8000 tonni_mutai/rochambeau
+docker run --rm -p 8000:8000 -e NODE_ENV='production' tonni_mutai/rochambeau
